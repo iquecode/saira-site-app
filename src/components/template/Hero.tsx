a0a8777b11a62fef5b6bc63d196a1c074b872da5
ique <ique@pop-os.localdomain>
2022-04-17 10:30:58 -0300

    corrections tipografy

--- src/components/template/Hero.tsx
+++ src/components/template/Hero.tsx
@@ -14,7 +14,7 @@ export default function Hero() {
         <div className="hero-content text-center text-gray-500">
             <div className="max-w-xg">
             <h1 className="mb-5 text-5xl font-bold">Por um mundo...</h1>
-            <p className="mb-5 py-6 text-3xl">...com respeito aos seres, integração com a natureza e decisões consentidas, para um vida plena, feliz e onde a evolução das potencialidades do ser humano é incentivada.</p>
+            <p className="mb-5 py-6 text-3xl">...com respeito aos seres, integração com a natureza e decisões consentidas, para uma vida plena, feliz e onde a evolução das potencialidades do ser humano é incentivada.</p>
             <button className="btn btn-info text-base-100 border-0 hover:bg-gray-600 w-64 m-4">Faça parte</button>
             <button className="btn btn-primary text-base-100 border-0 hover:bg-gray-600 w-64 m-4">LogIn</button>
             </div>
